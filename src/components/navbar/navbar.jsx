5ec57d27a0834deadcdba09d1c058d4d51ac75cf
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-06 10:58:35 -0300

    agrege los botones en nav

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,14 +1,22 @@
 import './navbar.css'
 import React from 'react'
+import { Stack, HStack, VStack } from '@chakra-ui/react'
 
 const Navbar = () =>{
 
 
     return(
         <>
-            <div>
-                
-            </div>
+            <HStack>
+                <nav>
+                    <h3>Portfolio</h3>
+                    <button>Home</button>
+                    <button>About</button>
+                    <button>Services</button>
+                    <button>Portfolio</button>
+                    <button>Contact Me</button>
+                </nav>
+            </HStack>
         </>
     )
 
