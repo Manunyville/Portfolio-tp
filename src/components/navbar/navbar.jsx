bd10a21a188babca45ac2852a8ccee17b015072c
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-06 11:15:06 -0300

    estilizando los botones

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,22 +1,28 @@
 import './navbar.css'
 import React from 'react'
-import { Stack, HStack, VStack } from '@chakra-ui/react'
+import { Stack, HStack, VStack, Flex, Button, ButtonGroup } from '@chakra-ui/react'
 
 const Navbar = () =>{
 
 
     return(
         <>
-            <HStack>
-                <nav>
+            <Flex justify={'center'} padding={30} >
+                <nav >
+                    <HStack gap={500} >
+                        <div>
                     <h3>Portfolio</h3>
+                        </div>
+                        <HStack gap={20}> 
                     <button>Home</button>
                     <button>About</button>
                     <button>Services</button>
-                    <button>Portfolio</button>
-                    <button>Contact Me</button>
+                    <button >Portfolio</button>
+                    <Button colorScheme='blue'>Contact me</Button>
+                        </HStack>
+                    </HStack>
                 </nav>
-            </HStack>
+            </Flex>
         </>
     )
 
