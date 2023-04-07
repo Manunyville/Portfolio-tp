0b2a89a2ca79ce837b601fde8804e501504700e0
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-07 19:58:43 -0300

    themes.js creado mas creado boton primario

---
+++ src/themes.js
@@ -0,0 +1,14 @@
+import { extendTheme } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
+export default extendTheme ({
+
+    Button : {
+        primary: {
+
+            color: "#061820",
+            border:"none",
+            bg:"#FFFFFF",
+            variant:"link",
+        }
+    }
+})
