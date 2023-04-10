2898b025ea690758caca0647b3361c538e117a03
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-10 19:53:39 -0300

    Importacion imagen, alineacion del componente about

--- src/components/aboutme/aboutMe.jsx
+++ src/components/aboutme/aboutMe.jsx
@@ -1,23 +1,27 @@
 import './aboutMe.css'
 import React from 'react'
-import { Stack, HStack, VStack, Flex, Button, ButtonGroup, Text, Container, Heading } from '@chakra-ui/react'
+import { Stack, HStack, VStack, Flex, Button, ButtonGroup, Text, Container, Heading, Image } from '@chakra-ui/react'
 import { motion } from "framer-motion"
+import Fotaki from '../../assets/img/fotoabout.png'
 
 const About = () =>{
 
     return(
-        <>
-            <Container marginTop={10}>
-                <HStack>
-                    <img src="" alt="" />
+        <Container>
+            <HStack marginTop={10}>
+                
+                    <Stack>
+
+                    <Image src={Fotaki} alt="" className='fotoAbout'width={400} height={200} />
+                    </Stack>
                     <VStack>
                         <Heading>About me</Heading>
                         <Heading fontSize='lg' color={'#6D7173'}> <strong>02 Months experience in Front end developing</strong></Heading>
                         <Text color={'#6D7173'}>Hello, I'am a Front end developer, my name is Manuel Nyville Vito. I am an experte in creating Web pages. I make pages really simple to use from complicated ideas.</Text>
                     </VStack>
-                </HStack>
-            </Container>
-        </>
+                
+            </HStack>
+        </Container>
     )
 }
 export default About
