0595084fe1ee14ba431cda518924c305d8a8477b
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-11 20:01:16 -0300

    estilizacion del componente y cards, falta cambiar imagenes

--- src/components/portfolio/myportfolio.jsx
+++ src/components/portfolio/myportfolio.jsx
@@ -1,46 +1,53 @@
 import './myportfolio.css'
 import React from 'react'
-import { Stack, HStack, VStack, Flex, Button, ButtonGroup, Text, Container, Heading, Card, CardHeader, CardBody,} from '@chakra-ui/react'
+import { Stack, HStack, VStack, Flex, Button, Image, Text, Container, Heading, Card, CardHeader, CardBody, CardFooter,} from '@chakra-ui/react'
 import { motion } from "framer-motion"
+import Fotaki from '../../assets/img/fotoabout.png'
+
 
 const Portfolio = () =>{
 
     return(
-        <Container>
+        <Container width={1200}>
             <VStack >
                 <VStack>
-                    <Text>Recent projects</Text>
+                    <Text color={'#0C7FB0'}>Recent projects</Text>
                     <Heading>My Portfolio</Heading>
                 </VStack>
                 <HStack>
-                    <Card  marginLeft={5} alignItems={'center'}  bg={'#F9F9F9'} width={600}>
+                    <Card width={300} height={350} bg={'#F9F9F9'} marginLeft={10}>
                         <CardHeader>
-                            
-                        <Heading size='md'> Website design</Heading>
+                        <Image src={Fotaki} height={150}/>
+                        <Heading size='md'> Twitter clon</Heading>
                         </CardHeader>
                         <CardBody>
-                        <Text>I create web sites.</Text>
+                        <Text>Is a Twitter profile as you were Messi</Text>
+                            <Button marginLeft={10}>View on github</Button>
                         </CardBody>
-                        
-                    </Card>
-                    <Card alignItems={'center'} marginLeft={5} bg={'#F9F9F9'} width={600}>
-                        <CardHeader>
                        
-                        <Heading size='md'>Application desing</Heading>
-                        </CardHeader>
-                        <CardBody>
-                        <Text>I create application as my client wishes.</Text>
-                        </CardBody>
                         
                     </Card>
-                    <Card bg={'#F9F9F9'}  marginLeft={5} alignItems={'center'} width={600}>
+                    <Card width={300} height={350} bg={'#F9F9F9'} marginLeft={10}>
                         <CardHeader>
-                        
-                        <Heading size='md'> Dashboard design</Heading>
+                        <Image src={Fotaki} height={150}/>
+                        <Heading size='md'> Forniture web</Heading>
                         </CardHeader>
                         <CardBody>
-                        <Text>I create Dashboard design.</Text>
+                        <Text>I create a web page for a forniture market</Text>
+                            <Button marginLeft={10}  as={motion.button} padding={3} whileHover={{  color:'#0C7FB0' }} color={'#F9F9F9'} bg={'#0C7FB0'} variant='solid'>View on github</Button>
                         </CardBody>
+                       
+                        
+                    </Card>
+                    <Card width={300} height={350} bg={'#F9F9F9'} marginLeft={10}>
+                        <CardHeader>
+                        <Image src={Fotaki} height={150}/>
+                        <Heading size='md'> Hotel page</Heading>
+                        </CardHeader>
+                        <CardBody>
+                        <Text>I create a page design for a hotel in Mar del plata.</Text>
+                            <Button marginLeft={10}>View on github</Button>
+                        </CardBody> 
                         
                     </Card>
                 </HStack>
