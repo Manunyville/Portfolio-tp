cee3f4e461e0aba1163f4eab1d58d34ae8665e2b
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-13 11:46:51 -0300

    componente feedback

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Choose from './components/Chooseme/chooseme';
 import About from './components/aboutme/aboutMe';
+import Testimonial from './components/feedback/feedback';
 import Hero from './components/hero/hero';
 import Navbar from './components/navbar/navbar';
 import Portfolio from './components/portfolio/myportfolio';
@@ -16,6 +17,7 @@ function App() {
     <Services/>
     <Portfolio/>
     <Choose/>
+    <Testimonial/>
     </div>
   );
 }
---
+++ src/components/feedback/feedback.jsx
@@ -0,0 +1,19 @@
+import './feedback.css'
+import React from 'react'
+import { Stack, HStack, VStack, Flex, Button, ButtonGroup, Text, Container, Heading, Image,Card, CardHeader, CardBody, } from '@chakra-ui/react'
+import { motion } from "framer-motion"
+
+const Testimonial = () =>{
+
+    return(
+    <Container>
+        <VStack>
+            <VStack>
+                <Text>Client's feedback</Text>
+            </VStack>
+        </VStack>
+    </Container>
+
+    )
+}
+export default Testimonial
