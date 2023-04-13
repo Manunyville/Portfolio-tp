13d87d580a83a6fbe06ca6cce25726ce312d0d11
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-13 11:28:16 -0300

    whileover agregado en las cards

--- src/components/Chooseme/chooseme.jsx
+++ src/components/Chooseme/chooseme.jsx
@@ -8,14 +8,14 @@ import Web from '../../assets/img/website.png'
 const Choose = () =>{
 
     return(
-        <Container width={1200}>
+        <Container width={1200} padding={10}>
             <VStack>
                 <VStack>
-                    <Text>Why you should choose me</Text>
+                    <Text color={'#0C7FB0'}>Why you should choose me</Text>
                     <Heading>Why choose me</Heading>
                 </VStack>
-                <HStack>
-                <Card  marginLeft={5} alignItems={'center'}  bg={'#F9F9F9'} width={300}>
+                <HStack gap={75}>
+                <Card  as={motion.div} whileHover={{ scale:1.1, color:'#0C7FB0' }}marginLeft={5} alignItems={'center'}  bg={'#F9F9F9'} width={250}>
                         <CardHeader>
                             <Image marginLeft={10} className='webImg' src={Web}/>
                         <Heading size='md'> Team player</Heading>
@@ -25,7 +25,7 @@ const Choose = () =>{
                         </CardBody>
                         
                     </Card>
-                    <Card alignItems={'center'} marginLeft={5} bg={'#F9F9F9'} width={300}>
+                    <Card as={motion.div} whileHover={{ scale:1.1, color:'#0C7FB0' }} alignItems={'center'} marginLeft={5} bg={'#F9F9F9'} width={250}>
                         <CardHeader>
                         <Image marginLeft={10} className='webImg' src={Web}/>
                         <Heading size='md'>Creative design</Heading>
@@ -35,7 +35,7 @@ const Choose = () =>{
                         </CardBody>
                         
                     </Card>
-                    <Card bg={'#F9F9F9'}  marginLeft={5} alignItems={'center'} width={300}>
+                    <Card as={motion.div} whileHover={{ scale:1.1, color:'#0C7FB0' }} bg={'#F9F9F9'}  marginLeft={5} alignItems={'center'} width={250}>
                         <CardHeader>
                         <Image marginLeft={10} className='webImg' src={Web}/>
                         <Heading size='md'> Responsive </Heading>
