a63d6badfec641d45eea624ed6f3577fb02c45b7
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-14 11:04:08 -0300

    centre img five stars

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -22,30 +22,30 @@ const Testimonial = () =>{
                         <Heading size='md'> Camila Moncada</Heading>
                         </CardHeader>
                         <CardBody>
-                            <Image src={Five} className='fiveStars'/>
+                            <Image src={Five}marginLeft={65} className='fiveStars'/>
                         <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus cum eligendi accusantium, sunt modi nihil asperiores vitae voluptas eum quia, officiis quae odio ab dolores veritatis vel perferendis. Fugit, illum.</Text>
                         </CardBody>
                         
                     </Card>
                     <Card  marginTop={10} as={motion.div} whileHover={{ scale:1.1, color:'#0C7FB0' }} alignItems={'center'} marginLeft={5} bg={'#F9F9F9'} width={250}>
                         <CardHeader>
-                        <Image marginLeft={10} className='webImg' src={Web}/>
+                        <Image  className='webImg' src={Web} marginLeft={5} />
                         <Heading size='md'>Lukas Olthoff</Heading>
                         </CardHeader>
                         <CardBody>
-                        <Image src={Five} className='fiveStars'/>
+                        <Image src={Five} className='fiveStars' marginLeft={65}/>
 
                         <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam esse soluta laudantium sed, minima ipsam corrupti tempora delectus, praesentium accusamus dolorum. Delectus saepe, ea beatae aliquid reiciendis doloremque quam mollitia!</Text>
                         </CardBody>
                         
                     </Card>
-                    <Card as={motion.div} whileHover={{ scale:1.1, color:'#0C7FB0' }} bg={'#F9F9F9'} j marginLeft={5} alignItems={'center'} width={250}>
-                        <CardHeader>
-                        <Image marginLeft={10} className='webImg' src={Web}/>
+                    <Card as={motion.div} whileHover={{ scale:1.1, color:'#0C7FB0' }} bg={'#F9F9F9'} justify={'center'} alignItems={'center'} width={250}>
+                        <CardHeader >
+                        <Image className='webImg' src={Web} marginLeft={45} />
                         <Heading size='md'>Marcos Servezinski </Heading>
                         </CardHeader>
                         <CardBody >
-                        <Image src={Five} className='fiveStars' marginLeft={40}/>
+                        <Image src={Five} className='fiveStars' marginLeft={65}/>
 
                         <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut rem praesentium sint sapiente alias libero laborum magnam quasi labore eos provident similique, accusantium nemo inventore repudiandae nulla sunt harum pariatur!</Text>
                         </CardBody>
