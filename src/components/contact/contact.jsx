f1d4f70fb16408bd1c3a3ec51de54e70a129a2b3
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-18 17:56:36 -0300

    Cambio de imagenes y texto de service.jsx

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,9 +1,9 @@
 import './contact.css'
 import React from 'react'
-import { Stack, HStack, VStack, Flex, Button, ButtonGroup, Text, Container, Heading, Image, Card, CardHeader, CardBody, Input,Textarea } from '@chakra-ui/react'
+import { Stack, HStack, VStack, Flex, Button, ButtonGroup, Text, Container, Heading, Image, Card, CardHeader, CardBody, Input, Textarea } from '@chakra-ui/react'
 import { motion } from "framer-motion";
 import { useForm, ValidationError } from '@formspree/react';
-import Azul from '../../assets/img/imgabstractaderecha.png';                   
+import Azul from '../../assets/img/imgabstractaderecha.png';
 
 
 
@@ -16,61 +16,61 @@ const Contact = () => {
         }
         return (
             <form action="https://formspree.io/f/mzbqknje" onSubmit={handleSubmit}>
-            <VStack>
-            <Input
-                    id="text"
-                    type="text"
-                    name="text"
-                    variant='filled' 
-                    borderRadius={5}
-                    placeholder='Full name'
-                    padding={25}
-                    width={450}
-                />
-                <Input
-                    id="email"
-                    type="email"
-                    name="email"
-                    variant='filled' 
-                    borderRadius={5}
-                    placeholder='Email'
-                    padding={25}
-                    width={450}
-                />
-                <ValidationError
-                    prefix="Email"
-                    field="email"
-                    errors={state.errors}
-                /> 
-                <Input
-                    id="message"
-                    name="message"
-                    variant='filled'
-                    borderRadius={5}
-                    placeholder='Message'
-                    height={100} 
-                    width={450}
-                
-                />
-                <ValidationError
-                    prefix="Message"
-                    field="message"
-                    errors={state.errors}
+                <VStack>
+                    <Input
+                        id="text"
+                        type="text"
+                        name="text"
+                        variant='filled'
+                        borderRadius={5}
+                        placeholder='Full name'
+                        padding={25}
+                        width={450}
                     />
-            </VStack>
-            <VStack justify={'center'}>
-                <Button type="submit" disabled={state.submitting}
-                marginTop={4}
-                as={motion.button}
-                width={150} 
-                whileHover={{ color: '#0C7FB0' }} 
-                color={'#F9F9F9'} 
-                bg={'#0C7FB0'} 
-                variant='solid'
-                justifyContent={'center'}>
-                    Submit
-                </Button>
-            </VStack>    
+                    <Input
+                        id="email"
+                        type="email"
+                        name="email"
+                        variant='filled'
+                        borderRadius={5}
+                        placeholder='Email'
+                        padding={25}
+                        width={450}
+                    />
+                    <ValidationError
+                        prefix="Email"
+                        field="email"
+                        errors={state.errors}
+                    />
+                    <Input
+                        id="message"
+                        name="message"
+                        variant='filled'
+                        borderRadius={5}
+                        placeholder='Message'
+                        height={100}
+                        width={450}
+
+                    />
+                    <ValidationError
+                        prefix="Message"
+                        field="message"
+                        errors={state.errors}
+                    />
+                </VStack>
+                <VStack justify={'center'}>
+                    <Button type="submit" disabled={state.submitting}
+                        marginTop={4}
+                        as={motion.button}
+                        width={150}
+                        whileHover={{ color: '#0C7FB0' }}
+                        color={'#F9F9F9'}
+                        bg={'#0C7FB0'}
+                        variant='solid'
+                        justifyContent={'center'}>
+                        Submit
+                    </Button>
+                </VStack>
             </form>
         );
     }
@@ -78,11 +78,11 @@ const Contact = () => {
         <Container id='contact' >
             <VStack >
                 <Text color={'#0C7FB0'}>Have something in your mind?</Text>
-                <Heading>Contact me</Heading> 
-                    <Stack gap={120} marginBottom={200} className='stackForm'>
-                        <ContactForm />
-       
-                    </Stack>
+                <Heading>Contact me</Heading>
+                <Stack gap={120} marginBottom={200} className='stackForm'>
+                    <ContactForm />
+
+                </Stack>
             </VStack>
         </Container>
     )
--- src/components/servicios/service.jsx
+++ src/components/servicios/service.jsx
@@ -2,7 +2,9 @@ import './service.css'
 import React from 'react'
 import { Stack, HStack, VStack, Flex, Button, ButtonGroup, Text, Container, Heading, Image, Card, CardHeader, CardBody, CardFooter } from '@chakra-ui/react'
 import { motion } from "framer-motion"
-import Web from '../../assets/img/website.png'
+import Web from '../../assets/img/codigoweb.png'
+import diseño from '../../assets/img/Diseñodepagina.png'
+import Diseñoweb from '../../assets/img/desarrolloweb.png'
 
 const Services = () =>{
  
@@ -15,33 +17,33 @@ const Services = () =>{
                     <Heading>My Services</Heading>
                 </VStack>
                 <HStack height={300} width={1000} gap={5}>
-                    <Card  marginLeft={5} alignItems={'center'}  bg={'#F9F9F9'} width={600}>
+                    <Card  marginLeft={5} alignItems={'center'}  bg={'#F9F9F9'} height={270}  width={600}>
                         <CardHeader>
                             <Image marginLeft={10} className='webImg' src={Web}/>
-                        <Heading size='md'> Website design</Heading>
+                        <Heading size='md'> Website developement </Heading>
                         </CardHeader>
                         <CardBody>
-                        <Text>I create web sites.</Text>
+                        <Text>Me encargo de la creacion de ideas complejas en paginas web sencillas de utilizar.</Text>
                         </CardBody>
                         
                     </Card>
-                    <Card alignItems={'center'} marginLeft={5} bg={'#F9F9F9'} width={600}>
+                    <Card alignItems={'center'} marginLeft={5} bg={'#F9F9F9'} height={270}  width={600}>
                         <CardHeader>
-                        <Image marginLeft={10} className='webImg' src={Web}/>
-                        <Heading size='md'>Application desing</Heading>
+                        <Image marginLeft={10} className='webImg' src={Diseñoweb} />
+                        <Heading size='md'>Website design</Heading>
                         </CardHeader>
                         <CardBody>
-                        <Text>I create application as my client wishes.</Text>
+                        <Text>Realizo los de las paginas de acuerdo a lo que el cliente desee. </Text>
                         </CardBody>
                         
                     </Card>
-                    <Card bg={'#F9F9F9'}  marginLeft={5} alignItems={'center'} width={600}>
+                    <Card bg={'#F9F9F9'}  marginLeft={5} alignItems={'center'} height={270} width={600}>
                         <CardHeader>
-                        <Image marginLeft={10} className='webImg' src={Web}/>
-                        <Heading size='md'> Dashboard design</Heading>
+                        <Image marginLeft={10} className='webImg' src={diseño}/>
+                        <Heading size='md'>Web manteinance </Heading>
                         </CardHeader>
                         <CardBody>
-                        <Text>I create Dashboard design.</Text>
+                        <Text>Hago actividades de mantenimiento y optimizacion de paginas web, para mejorar su funcionamiento y prevencion.</Text>
                         </CardBody>
                         
                     </Card>
