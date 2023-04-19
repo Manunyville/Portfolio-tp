f2aa2f11f28159c47dbe8ff4a16bb0e58827b0e9
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-19 20:39:04 -0300

    cambio en el feedback

--- src/components/feedback/feedback.jsx
+++ src/components/feedback/feedback.jsx
@@ -7,74 +7,67 @@ import Five from '../../assets/img/fivestars.png'
 import Comillas from '../../assets/img/comillas.png'
 import Azul from '../../assets/img/imgabstractaizquierda.png'
 import { FaUserLock } from 'react-icons/fa'
+import marcos from '../../assets/img/Marcos.jpg'
+import lukas from '../../assets/img/Lukas.jpg'
+import cami from '../../assets/img/Camila.jpeg'
+
 
 const Testimonial = () => {
 
     return (
-        <Container id='testimonios'>
-            <VStack>
-                <VStack margin={5}>
-                    <Text color={'#0C7FB0'}>Client's feedback</Text>
-                </VStack>
-                    <Heading>Testimonials</Heading>
-                <HStack gap={30} className='stackCards' width={1200}>
-                    <VStack>
-             <Image src={Azul} width={100} height={'auto'} marginBottom={250}/>                   
 
-                    </VStack>
-                        <Card as={motion.div} whileHover={{ scale: 1.1, color: '#0C7FB0' }} alignItems={'center'} bg={'#F9F9F9'} width={400} height={300}>
-
-                            <HStack gap={10} marginLeft={85}>
-
-                                <Image marginLeft={10} className='webImg' src={Web} />
-                                <Image src={Comillas} w={100} />
-                            </HStack>
-
-                            <CardBody>
-                                <Heading size='md' marginLeft={75}> Camila Moncada</Heading>
-                                <Image src={Five} marginLeft={100} className='fiveStars' />
-                                <Text>Manuel realiza trabajos muy buenos, todos fueron entregados en tiempo y forma; ademas trabaja muy bien con su equipo, sin duda volveria a contratarlos</Text>
-                            </CardBody>
-
-                        </Card>
-
-                        <Card as={motion.div} whileHover={{ scale: 1.1, color: '#0C7FB0' }} alignItems={'center'} marginTop={25} marginLeft={5} bg={'#F9F9F9'} width={400} height={300}>
-                            <HStack gap={10} marginLeft={85}>
-                                <Image className='webImg' src={Web} marginLeft={5} />
-                                <Image src={Comillas} w={100} />
-                            </HStack>
-                            <CardBody justifyContent={'center'}>
-                                <Heading size='md' marginLeft={75}>Lukas Olthoff</Heading>
-                                <Image src={Five} className='fiveStars' marginLeft={100} />
-
-                                <Text>Los trabajos realizados por Manuel fueron increibles, mucha responsabilidad de su parte y siempre muy creativo!</Text>
-                            </CardBody>
-
-                        </Card>
-
-                        <Card as={motion.div} whileHover={{ scale: 1.1, color: '#0C7FB0' }} bg={'#F9F9F9'} marginTop={25} alignItems={'center'} width={400} height={300}>
-                            <HStack gap={10} marginLeft={85}>
-                                <Image className='webImg' src={Web} marginLeft={45} />
-                                <Image src={Comillas} w={100} />
-
-                            </HStack>
-                            <CardBody justify={'center'}>
-                                <Heading size='md' marginLeft={55}>Marcos Servezinski </Heading>
-                                <Image src={Five} className='fiveStars' marginLeft={100} />
-
-                                <Text>Los trabajos que Manuel ha hecho son muy ingenios, las ideas que yo le dije el las tranformo tal cual yo me las imaginaga!</Text>
-                            </CardBody>
-
-                        </Card>
-                    </HStack>
-                
+        <Stack id='testimonios' width={'full'} margin="0 auto" marginLeft="105px">
+            <VStack width="1200px" gap="45px" marginLeft="70px">
                 <VStack>
-                    <div>aca necesito un carrusel para mas opiniones</div>
-                    <Button marginTop={4} as={motion.button} paddingX={10} whileHover={{ color: '#0C7FB0' }} color={'#F9F9F9'} bg={'#0C7FB0'} variant='solid'> View All</Button>
-
+                    <Text color={'#0C7FB0'}>Client's feedback</Text>
+                    <HStack>
+                        <Image src={Azul} className='imgabstractaFeed' />
+                    </HStack>
+                    <Stack>
+                        <Heading>Testimonials</Heading>
+                    </Stack>
                 </VStack>
+                <HStack gap="30px" className='stackFeed' marginLeft="45px" margin="0 auto">
+
+                    <Card as={motion.div} whileHover={{ scale: 1.1, color: '#0C7FB0' }} className='cardsFeedback' bg={'#F9F9F9'} width='300px' height='300px'>
+                        <HStack gap={10}>
+                            <Image className='imgFeed' src={cami} />
+                            <Image src={Comillas} w='100px' className='comillasfeed'/>
+                        </HStack>
+                        <CardBody className='cardsFeedback'>
+                            <Heading size='md'> Camila Moncada</Heading>
+                            <Image src={Five} className='fiveStars' />
+                            <Text>Manuel realiza trabajos muy buenos, todos fueron entregados en tiempo y forma; ademas trabaja muy bien con su equipo, sin duda volveria a contratarlos</Text>
+                        </CardBody>
+                    </Card>
+
+                    <Card as={motion.div} whileHover={{ scale: 1.1, color: '#0C7FB0' }} className='cardsFeedback' bg={'#F9F9F9'} width='300px' height='300px'>
+                        <HStack >
+                            <Image className='imgFeed' src={lukas} />
+                            <Image src={Comillas} w='100px' />
+                        </HStack>
+                        <CardBody className='cardsFeedback'>
+                            <Heading size='md'>Lukas Olthoff</Heading>
+                            <Image src={Five} className='fiveStars' />
+                            <Text>Los trabajos realizados por Manuel fueron increibles, mucha responsabilidad de su parte y siempre muy creativo!</Text>
+                        </CardBody>
+                    </Card>
+
+                    <Card as={motion.div} whileHover={{ scale: 1.1, color: '#0C7FB0' }} bg={'#F9F9F9'} className='cardsFeedback' width='300px' height='300px'>
+                        <HStack >
+                            <Image className='imgFeed' src={marcos} />
+                            <Image src={Comillas} w='100px' />
+
+                        </HStack>
+                        <CardBody className='cardsFeedback'>
+                            <Heading size='md'>Marcos Servezinski </Heading>
+                            <Image src={Five} className='fiveStars' />
+                            <Text>Los trabajos que Manuel ha hecho son muy ingenios, las ideas que yo le dije el las tranformo tal cual yo me las imaginaga!</Text>
+                        </CardBody>
+                    </Card>
+                </HStack>
             </VStack>
-        </Container>
+        </Stack>
 
     )
 }
