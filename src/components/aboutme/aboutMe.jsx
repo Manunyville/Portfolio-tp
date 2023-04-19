6d6a411ec6787242cb156850e60197a08442a0ac
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-18 21:56:43 -0300

    actualizaciones en el aboutme.jsx

--- src/components/aboutme/aboutMe.jsx
+++ src/components/aboutme/aboutMe.jsx
@@ -2,26 +2,29 @@ import './aboutMe.css'
 import React from 'react'
 import { Stack, HStack, VStack, Flex, Button, ButtonGroup, Text, Container, Heading, Image } from '@chakra-ui/react'
 import { motion } from "framer-motion"
-import Fotaki from '../../assets/img/fotoabout.png'
+import Fotaki from '../../assets/img/Fotoportfolio.png'
 
 const About = () =>{
 
     return(
-        <Container width={1200} id='about'>
-            <HStack marginTop={10} justify={'left'}>
+        <Stack id='about' width={'full'} bg={'#F9F9F9'}>
+            <HStack width={1200} marginTop={10} justify={'space-around'} gap={110}>
                 
-                    <Stack>
+                    <HStack marginLeft={220}>
 
-                    <Image src={Fotaki} alt="" className='fotoAbout'width={400} height={200} />
-                    </Stack>
+                    <Image src={Fotaki} width={450} height={400} className='fotoAbout' />
+                    </HStack>
                     <VStack >
+                        <Stack>
                         <Heading>About me</Heading>
-                        <Heading fontSize='lg' color={'#6D7173'}> <strong>02 Months experience in Front end developing</strong></Heading>
-                        <Text color={'#6D7173'}>Hello, I'am a Front end developer, my name is Manuel Nyville Vito. I am an experte in creating Web pages. I make pages really simple to use from complicated ideas.</Text>
+                        <Heading size='lg' color={'#6D7173'}> 02 Meses de experiencia en desarrolo Front end</Heading>
+                        <Text color={'#6D7173'}>Hola, yo soy Manuel, un desarrollador Front end, me encargo del desarrollo de paginas web y aplicaciones web de acuerdo a lo que el cliente proponga. Transformo ideas complicadas en proyecto faciles de usar. </Text>
+
+                        </Stack>
                     </VStack>
                 
             </HStack>
-        </Container>
+        </Stack>
     )
 }
 export default About
