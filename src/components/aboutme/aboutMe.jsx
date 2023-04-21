799011ce7e71456184b85eda294e8015effe0cfc
Manunyville <125371151+Manunyville@users.noreply.github.com>
2023-04-21 03:36:00 -0300

    responsive en about

--- src/components/aboutme/aboutMe.jsx
+++ src/components/aboutme/aboutMe.jsx
@@ -8,18 +8,24 @@ const About = () => {
 
     return (
 
-        <Stack width='full' id='about' margin="0" bg={'#F9F9F9'} >
-            <HStack w="1200px" gap="120px" marginTop="15px">
+        <Stack width='full' id='about' margin="0 auto" bg={'#F9F9F9'} >
+            <HStack w={{xl:"1200px",md:'400px',sm:'350px',base:'250px' }}
+            gap={{xl:"120px", md:'75px', sm:'45px',base:'15px' }}
+            marginTop="15px" 
+            flexDirection={{xl:'row',md:'column',sm:'column',base:'column'}}>
 
 
-                <HStack marginLeft="220px">
-                    <Image src={Foto} w="500px" h="400px" />
+                <HStack marginLeft={{xl:"220px",md:'110px',sm:'55px',base:'0px'}}>
+                    <Image src={Foto} 
+                     w={{xl:"500px", md:'250px', sm:'75px', base:'25px'}} 
+                    h={{xl:"400px", md:'200px', sm:'65px', base:'15px'}} />
                 </HStack>
-                <VStack>
+                <VStack > 
                     <Stack>
-                        <Heading>About me</Heading>
-                        <Heading size='lg' color={'#6D7173'}> 02 Meses de experiencia en desarrolo Front end</Heading>
-                        <Text color={'#6D7173'}>Hola, yo soy Manuel, un desarrollador Front end, me encargo del desarrollo de paginas web y aplicaciones web de acuerdo a lo que el cliente proponga. Transformo ideas complicadas en proyecto faciles de usar. </Text>
+                        <Heading fontSize={{xl:'45px',md:'35px',sm:'20px',base:'5px'}}>About me</Heading>
+                        <Heading fontSize={{xl:'30px',md:'20px',sm:'10px',base:'5px'}} 
+                        color={'#6D7173'}> 02 Meses de experiencia en desarrolo Front end</Heading>
+                        <Text color={'#6D7173'} fontSize={{xl:'15px',md:'10px',sm:'8px',base:'5px'}}>Hola, yo soy Manuel, un desarrollador Front end, me encargo del desarrollo de paginas web y aplicaciones web de acuerdo a lo que el cliente proponga. Transformo ideas complicadas en proyecto faciles de usar. </Text>
                     </Stack>
                 </VStack>
 
